f7477e89a23b780e56707c00ea78b77f852c713b
Roi Holgarsson <rho@formula.fo>
2019-12-03 16:28:02 +0000

    Part 2 setup

---
+++ AdventOfCode/Day2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] input = await File.ReadAllLinesAsync(Path.Combine(Directory.GetCurrentDirectory(), "Puzzles", "puzzle2_0.txt"));
+
+            // First part
+
+        }
+    }
+}
